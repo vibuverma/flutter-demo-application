911449d17bae9d3d306fed35830fc080173786d5
Vaibhav Verma <vaibhavverma016@gmail.com>
2022-01-14 15:47:47 +0530

    future delay and animated container

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -11,6 +11,7 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   String name = "";
+  bool changeButton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -60,24 +61,36 @@ class _LoginPageState extends State<LoginPage> {
                   ),
 
                   InkWell(
-                    onTap: () {
+                    onTap: () async {
+                      setState(() {
+                        changeButton = true;
+                      });
+
+                      await Future.delayed(Duration(seconds: 1));
                       Navigator.pushNamed(context, MyRoutes.homeRoute);
                     },
-                    child: Container(
-                        width: 150,
+                    child: AnimatedContainer(
+                        duration: Duration(seconds: 1),
+                        width: changeButton ? 50 : 150,
                         height: 40,
                         alignment: Alignment.center,
-                        child: Text(
-                          "Login",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 30,
-                          ),
-                        ),
+                        child: changeButton
+                            ? Icon(
+                                Icons.done,
+                                color: Colors.white,
+                              )
+                            : Text(
+                                "Login",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 30,
+                                ),
+                              ),
                         decoration: BoxDecoration(
                           color: Colors.deepPurpleAccent,
-                          borderRadius: BorderRadius.circular(7),
+                          borderRadius:
+                              BorderRadius.circular(changeButton ? 50 : 7),
                         )),
                   ),
                   // ElevatedButton(
