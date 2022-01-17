4740f968bce93b8b357ff34420205764bf6e05f3
Vaibhav Verma <vaibhavverma016@gmail.com>
2022-01-17 18:35:33 +0530

    themes.dart added to manage themes

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1_scratch/utils/routes.dart';
 import 'pages/home_page.dart';
 import 'pages/login_page.dart';
+import 'package:flutter_application_1_scratch/widgets/themes.dart';
 // import 'pages/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -16,25 +17,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        fontFamily: GoogleFonts.poppins().fontFamily,
-        primaryTextTheme: GoogleFonts.latoTextTheme(),
-        appBarTheme: AppBarTheme(
-          color: Colors.white,
-          elevation: 0,
-          iconTheme: IconThemeData(color: Colors.black),
-          titleTextStyle: Theme.of(context).textTheme.headline6,
-        ),
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        primarySwatch: Colors.deepPurple,
-        fontFamily: GoogleFonts.lato().fontFamily,
-        primaryTextTheme: GoogleFonts.latoTextTheme(),
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
       debugShowCheckedModeBanner: false,
       initialRoute: MyRoutes.homeRoute,
       routes: {
---
+++ lib/widgets/themes.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyTheme {
+  static ThemeData lightTheme(BuildContext context) => ThemeData(
+      primarySwatch: Colors.blue,
+      fontFamily: GoogleFonts.poppins().fontFamily,
+      primaryTextTheme: GoogleFonts.latoTextTheme(),
+      appBarTheme: AppBarTheme(
+        color: Colors.white,
+        elevation: 0,
+        iconTheme: IconThemeData(color: Colors.black),
+        titleTextStyle: Theme.of(context).textTheme.headline6,
+      ));
+
+  static ThemeData darkTheme(BuildContext context) => ThemeData(
+      primarySwatch: Colors.blue,
+      fontFamily: GoogleFonts.poppins().fontFamily,
+      primaryTextTheme: GoogleFonts.latoTextTheme(),
+      appBarTheme: AppBarTheme(
+        color: Colors.black,
+        elevation: 0,
+        iconTheme: IconThemeData(color: Colors.white),
+        titleTextStyle: Theme.of(context).textTheme.headline6,
+      ));
+}
