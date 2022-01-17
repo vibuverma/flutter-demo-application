57fd1b518951d54d51ab65e5726f1d60e84078b5
Vaibhav Verma <vaibhavverma016@gmail.com>
2022-01-17 18:43:11 +0530

    making catalog

---
+++ lib/models/catalog.dart
@@ -0,0 +1,20 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item({this.id, this.name, this.desc, this.price, this.color, this.image});
+}
+
+
+final product = [Item(
+  id: "Product1",
+  name: "iPhone 12 Pro",
+  desc: "iPhone 12 Pro is the latest in the iPhone series. It features a new all-screen design, a glass body, and a notch-like bezel. It is powered by Apple's dual-core A13 Bionic processor and has a new 12MP rear camera with Portrait mode, and a new 12MP front camera with Portrait mode. It has a new dual-camera system with Portrait mode, and a new TrueDepth camera with Portrait mode. It has a new dual-camera system with Portrait mode, and a new TrueDepth camera with Portrait mode.",
+  price: 699,
+  color: "#33505a",
+  image: "assets/images/iphone12.png",
+)]
