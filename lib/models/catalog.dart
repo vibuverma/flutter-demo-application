26042dc8623c3fe5801746b8796600251f8c7d40
Vaibhav Verma <vaibhavverma016@gmail.com>
2022-01-19 18:57:59 +0530

    catalog.dart error solved

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,22 +1,31 @@
+class CatalogModel {
+  static final items = [
+    Item(
+      id: 1,
+      name: "iPhone 12 Pro",
+      desc:
+          "iPhone 12 Pro is the latest in the iPhone series. It features a new all-screen design, a glass body, and a notch-like bezel. It is powered by Apple's dual-core A13 Bionic processor and has a new 12MP rear camera with Portrait mode, and a new 12MP front camera with Portrait mode. It has a new dual-camera system with Portrait mode, and a new TrueDepth camera with Portrait mode. It has a new dual-camera system with Portrait mode, and a new TrueDepth camera with Portrait mode.",
+      price: 699,
+      color: "#33505a",
+      image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc",
+    )
+  ];
+}
+
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
   final String color;
   final String image;
 
-  Item({this.id, this.name, this.desc, this.price, this.color, this.image});
-}
-
-final product = [
   Item(
-    id: "Product1",
-    name: "iPhone 12 Pro",
-    desc:
-        "iPhone 12 Pro is the latest in the iPhone series. It features a new all-screen design, a glass body, and a notch-like bezel. It is powered by Apple's dual-core A13 Bionic processor and has a new 12MP rear camera with Portrait mode, and a new 12MP front camera with Portrait mode. It has a new dual-camera system with Portrait mode, and a new TrueDepth camera with Portrait mode. It has a new dual-camera system with Portrait mode, and a new TrueDepth camera with Portrait mode.",
-    price: 699,
-    color: "#33505a",
-    image: "assets/images/iphone12.png",
-  )
-];
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.price,
+      required this.color,
+      required this.image});
+}
